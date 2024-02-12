11fcacdf7f3c6d77fbe4f8d61ced2bcf78907582
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-02-12 17:29:24 +0700

    Initial commit

---
+++ lib/Module/Pluggable/_ModuleFeatures.pm
@@ -0,0 +1,37 @@
+## no critic: TestingAndDebugging::RequireUseStrict
+package Module::Pluggable::_ModuleFeatures;
+
+#IFUNBUILT
+use strict;
+use warnings;
+#END IFUNBUILT
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our %FEATURES = (
+    module_v => 5.2,
+    features => {
+        PluginSystem => {
+            can_let_plugin_contain_multiple_handlers => 1,
+
+            can_let_plugin_skip_event => 0,
+            can_let_plugin_skip_other_plugins => 0,
+            can_let_plugin_repeat_event => 0,
+            can_let_plugin_repeat_other_plugins => 0,
+
+            can_put_handler_in_other_events => 0,
+            can_handler_priority => 0,
+            can_customize_handler_priority => 0,
+            can_plugin_configuration => 1,
+            can_add_multiple_handlers_from_a_plugin => 0,
+        },
+    },
+1;
+# ABSTRACT: Features declaration for Module::Pluggable
+
+=head1 SEE ALSO
+
+L<Module::Features>
